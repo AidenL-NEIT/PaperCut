7543d74544e9c6d43bb1fba1180ccc6efe114023
Aiden Langley <me@aidenlangley.com>
2020-10-07 19:16:12 +1300

    Reformatting

--- CheckAndStartPaperCut.ps1
+++ CheckAndStartPaperCut.ps1
@@ -1,11 +1,9 @@
 param ($ConnectTo, $Cache)
 if (Test-Connection $ConnectTo) {
     $path = $env:LOCALAPPDATA + "\temp";
-
     $papercut = Get-Process pc-client -ErrorAction SilentlyContinue;
     if (!$papercut) {
         Start-Process -FilePath $Cache -ArgumentList "--silent", "--noquit", "--cache $path";
     }
-
     Remove-Variable papercut;
 }
